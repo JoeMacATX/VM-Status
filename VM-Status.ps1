ed9ad2120c8153041c380c173cc7bde386b63dc1
JoeMacATX <36079229+JoeMacATX@users.noreply.github.com>
2018-02-05 17:39:45 -0600

    Changed Azure metric calls, cleanedup output
    Used latest version of metric calls to pull more data at once resulting in faster script and fewer calls to azure.  Cleaned up output tables by adding function to make numbers more user friending (k,M,G,TB)

--- VM-Status.ps1
+++ VM-Status.ps1
@@ -1,10 +1,10 @@
-﻿az login                         
-az account set --subscription "Visual Studio Enterprise: BizSpark"
+﻿Login-AzureRMAccount
 
 Clear
 # This section contains baseline information relevant only to my subscription.  Can be modified/deleted as needed
 $NoteA = "Need to look at commonizing to minimal OS versions, i.e. migrate all Windows to DataCenter 2016"
-$NoteB = "30d Baseline values as of Jan 28, 2018"
+$NoteB = "30d Baseline values as of Jan 28, 2018`n"
+$NoteC = "Network Baselines are 1-day averages from week ending Jan 28, 2018"
 
 $FileDate = (Get-Date).ToString("yyyy-MM-dd_HHmm")
 Write $FileDate | Out-File C:\Temp\$($FileDate)_Azure_Status.txt -append
@@ -19,96 +19,116 @@ return $Item
 }
 
 $Baselines = New-Object System.Collections.ArrayList
-$Baselines += AddToBaseline 'WMDev' 'CPU30d' 1.6
-$Baselines += AddToBaseline 'LAZDev' 'CPU30d' 4.88
-$Baselines += AddToBaseline 'CygnalDev' 'CPU30d' 0.13
-$Baselines += AddToBaseline 'WMReporting' 'CPU30d' 12.1
+$Baselines += AddToBaseline 'WMDev' 'CPU30d' '1.60'            # CPU 30d baselines are from Azure 30d graphs, ending Jan 28, 2018
+$Baselines += AddToBaseline 'LAZDev' 'CPU30d' '4.88'
+$Baselines += AddToBaseline 'CygnalDev' 'CPU30d' '0.13'
+$Baselines += AddToBaseline 'WMReporting' 'CPU30d' '12.10'
+$Baselines += AddToBaseline 'CygnalDev' 'NetIn_1Day' 68300000    # Net Baselines are 7d / 7 average ending Jan 28, 2018
+$Baselines += AddToBaseline 'CygnalDev' 'NetOut_1Day' 63200000   
+$Baselines += AddToBaseline 'LAZDev' 'NetIn_1Day' 770000000
+$Baselines += AddToBaseline 'LAZDev' 'NetOut_1Day' 1008000000   
+$Baselines += AddToBaseline 'WMDev' 'NetIn_1Day' 117700000
+$Baselines += AddToBaseline 'WMDev' 'NetOut_1Day' 210000000   
+$Baselines += AddToBaseline 'WMReporting' 'NetIn_1Day' 59900000
+$Baselines += AddToBaseline 'WMReporting' 'NetOut_1Day' 127600000   
 
 # End notes section
 
-Write "VM-List" | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
-$VMTableAzureOutput = az vm list --query "[].{VMName:name, ResGroup:resourceGroup, OSType:storageProfile.imageReference.sku, Size:hardwareProfile.vmSize, Location:location, ID:id}"  --out table
-$VMTableAzureOutput | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
-
-$MyVMList = @()
-for ($i = 2; $i -lt $VMTableAzureOutput.Length; $i++)         # First row of table is column headers [0], second row is "-"s [1]
+function ConvertBytes($value)
 {
-   $SplitVals = $VMTableAzureOutput[$i] -split "\s+"
-   $MyVMList += $SplitVals[5]                                 #$VMList contains the .ID values in one column
+if ($value -lt 1000) {return $value.ToString("0")}
+if ($value -lt 1000000) {return $($value / 1000).ToString("0.0") + "k"}
+if ($value -lt 1000000000) {return $($value / 1000000).ToString("0.0") + "M"}
+if ($value -lt 1000000000000) {return $($value / 1000000000).ToString("0.0") + "G"}
+if ($value -lt 1000000000000000) {return $($value / 1000000000000).ToString("0.0") + "T"}
+return ("Err Conversion")
 }
-Write "`n" | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+
+Write "`nVM-List" | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+$VMList = Get-AzureRmVM
+$MyVMArray = New-Object System.Collections.ArrayList
+foreach ($vm in $VMList)
+{
+    $Entry = New-Object System.Object
+    $Entry | Add-Member -MemberType NoteProperty -Name "VMName" -Value $vm.Name
+    $Entry | Add-Member -MemberType NoteProperty -Name "ResGroup" -Value $vm.ResourceGroupName
+    $Entry | Add-Member -MemberType NoteProperty -Name "OSType" -Value $vm.storageProfile.imageReference.sku
+    $Entry | Add-Member -MemberType NoteProperty -Name "Size" -Value $vm.HardwareProfile.VmSize
+    $Entry | Add-Member -MemberType NoteProperty -Name "Location" -Value $vm.Location
+    $Entry | Add-Member -MemberType NoteProperty -Name "ID" -Value $vm.Id
+
+    $MyVMArray.Add($Entry) | out-null
+}
+
+$MyVMArray | Select-Object VMName,ResGroup,OSType,Size | Format-Table -autosize | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
 $NoteA | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
 
 
-$OutputTable = New-Object System.Collections.ArrayList
-Write-Host -NoNewline "`nCalculating CPU Usage."              #Does not output to Text File
-Write "`n`nCalculating CPU Usage..." | Out-File C:\Temp\$($FileDate)_Azure_Status.txt -append
-foreach ($SingleVM in $MyVMList)                     #SingleVM is the .id value
+$My_CPU_OutputTable = New-Object System.Collections.ArrayList
+$My_NetIn_OutputTable = New-Object System.Collections.ArrayList
+$My_NetOut_OutputTable = New-Object System.Collections.ArrayList
+Write-Host -NoNewline "`nCalculating Usage Metrics"              #Does not output to Text File
+Write "`n`Usage Metrics..." | Out-File C:\Temp\$($FileDate)_Azure_Status.txt -append
+$MetricList = @("Percentage CPU", "Network In", "Network Out")
+$EndTime = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+$Time5MinAgo = (Get-Date).AddMinutes(-5).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+$Time1HrAgo = (Get-Date).AddMinutes(-60).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+$Time6HrAgo = (Get-Date).AddHours(-6).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+$Time12HrAgo = (Get-Date).AddHours(-12).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+$Time1DayAgo = (Get-Date).AddDays(-1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+foreach ($SingleVM in $MyVMArray)                     #SingleVM is the .id value
 {
-    $Entry = New-Object System.Object
-    $EndTime = (Get-Date).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-    $Time5MinAgo = (Get-Date).AddMinutes(-5).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-    $Time6HrAgo = (Get-Date).AddHours(-6).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-    $Time12HrAgo = (Get-Date).AddHours(-12).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-    $Time1DayAgo = (Get-Date).AddDays(-1).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
-    $temp = $SingleVM -split '/'                     #temp is an array with each level of the .id directory tree
-    $VMName = $temp[8]                               #short VMName is the last 8th place in directory tree, before leading / counts as 0
     Write-Host -NoNewline "."
-    $ReturnedData = az monitor metrics list --resource $SingleVM --metric "Percentage CPU" --interval "PT5M" --start-time $Time5MinAgo --end-time $EndTime --aggregation Average Maximum
-    $DataAverage = $ReturnedData | Select-String -pattern 'average'
-    $DataAverage = $DataAverage -replace "[^0-9.]",''
-    $DataMax = $ReturnedData | Select-String -pattern 'maximum'
-    $DataMax = $DataMax -replace "[^0-9.]",''
-    $FiveMinCPU = [math]::round($DataAverage,2)
-    $FiveMinCPUMax = [math]::round($DataMax,2)
-    #intentionally overwrites $data with a new metric call
-    Write-Host -NoNewline "."
-    $ReturnedData = az monitor metrics list --resource $SingleVM --metric "Percentage CPU" --interval "PT1H" --aggregation Average Maximum
-    $DataAverage = $ReturnedData | Select-String -pattern 'average'
-    $DataAverage = $DataAverage -replace "[^0-9.]",''
-    $DataMax = $ReturnedData | Select-String -pattern 'maximum'
-    $DataMax = $DataMax -replace "[^0-9.]",''
-    $OneHrCPU = [math]::round($DataAverage,2)
-    $OneHrCPUMax = [math]::round($DataMax,2)
-    #intentionally overwrites $data with a new metric call
-    Write-Host -NoNewline "."
-    $ReturnedData = az monitor metrics list --resource $SingleVM --metric "Percentage CPU" --interval "PT6H" --start-time $Time6HrAgo --end-time $EndTime --aggregation Average Maximum
-    $DataAverage = $ReturnedData | Select-String -pattern 'average'
-    $DataAverage = $DataAverage -replace "[^0-9.]",''
-    $DataMax = $ReturnedData | Select-String -pattern 'maximum'
-    $DataMax = $DataMax -replace "[^0-9.]",''
-    $SixHrCPU = [math]::round($DataAverage,2)
-    $SixHrCPUMax = [math]::round($DataMax,2)
-    #intentionally overwrites $data with a new metric call
-    Write-Host -NoNewline "."
-    $ReturnedData = az monitor metrics list --resource $SingleVM --metric "Percentage CPU" --interval "PT12H" --start-time $Time12HrAgo --end-time $EndTime --aggregation Average Maximum
-    $DataAverage = $ReturnedData | Select-String -pattern 'average'
-    $DataAverage = $DataAverage -replace "[^0-9.]",''
-    $DataMax = $ReturnedData | Select-String -pattern 'maximum'
-    $DataMax = $DataMax -replace "[^0-9.]",''
-    $TwelveHrCPU = [math]::round($DataAverage,2)
-    $TwelveHrCPUMax = [math]::round($DataMax,2)
-    #intentionally overwrites $data with a new metric call
-    Write-Host -NoNewline "."
-    $ReturnedData = az monitor metrics list --resource $SingleVM --metric "Percentage CPU" --interval "P1D" --start-time $Time1DayAgo --end-time $EndTime --aggregation Average Maximum
-    $DataAverage = $ReturnedData | Select-String -pattern 'average'
-    $DataAverage = $DataAverage -replace "[^0-9.]",''
-    $DataMax = $ReturnedData | Select-String -pattern 'maximum'
-    $DataMax = $DataMax -replace "[^0-9.]",''
-    $OneDayCPU = [math]::round($DataAverage,2)
-    $OneDayCPUMax = [math]::round($DataMax,2)
-    $Entry | Add-Member -MemberType NoteProperty -Name "   VMName   " -Value $VMName
-    $Entry | Add-Member -MemberType NoteProperty -Name "5MinCPU / Max" -value ("$($FiveMinCPU)".padleft(8) + " / " + "$($FiveMinCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "OneHrCPU / Max" -value ("$($OneHrCPU)".padleft(8) + " / " + "$($OneHrCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "SixHrCPU / Max" -value ("$($SixHrCPU)".padleft(8) + " / " + "$($SixHrCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "TwelveHrCPU / Max" -value ("$($TwelveHrCPU)".padleft(8) + " / " + "$($TwelveHrCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "OneDayCPU / Max" -value ("$($OneDayCPU)".padleft(8) + " / " + "$($OneDayCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "30d Baseline" -value $($Baselines | where-object{$_.VM -eq $VMName -and $_.Metric -eq "CPU30d"} | select -expand "Value")
-    $OutPutTable.Add($Entry) | out-null
-    Write-Host -NoNewline "."
+    $CPUEntry = New-Object System.Object
+    $NetInEntry = New-Object System.Object
+    $NetOutEntry = New-Object System.Object
+#next few lines return array of values in order of $MetricList in each of the intervals specified
+    $Metrics5MinAvg = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 00:05:00 -MetricName $MetricList -StartTime $Time5MinAgo -EndTime $EndTime -AggregationType Average -WarningAction SilentlyContinue  
+    $Metrics5MinMax = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 00:05:00 -MetricName $MetricList -StartTime $Time5MinAgo -EndTime $EndTime -AggregationType Maximum -WarningAction SilentlyContinue
+    $Metrics5MinTot = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 00:05:00 -MetricName $MetricList -StartTime $Time5MinAgo -EndTime $EndTime -AggregationType Total -WarningAction SilentlyContinue
+    $Metrics1HrAvg = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 01:00:00 -MetricName $MetricList -StartTime $Time1HrAgo -EndTime $EndTime -AggregationType Average -WarningAction SilentlyContinue
+    $Metrics1HrMax = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 01:00:00 -MetricName $MetricList -StartTime $Time1HrAgo -EndTime $EndTime -AggregationType Maximum -WarningAction SilentlyContinue
+    $Metrics1HrTot = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 01:00:00 -MetricName $MetricList -StartTime $Time1HrAgo -EndTime $EndTime -AggregationType Total -WarningAction SilentlyContinue
+    $Metrics6HrAvg = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 06:00:00 -MetricName $MetricList -StartTime $Time6HrAgo -EndTime $EndTime -AggregationType Average -WarningAction SilentlyContinue
+    $Metrics6HrMax = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 06:00:00 -MetricName $MetricList -StartTime $Time6HrAgo -EndTime $EndTime -AggregationType Maximum -WarningAction SilentlyContinue
+    $Metrics6HrTot = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 06:00:00 -MetricName $MetricList -StartTime $Time6HrAgo -EndTime $EndTime -AggregationType Total -WarningAction SilentlyContinue
+    $Metrics12HrAvg = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 12:00:00 -MetricName $MetricList -StartTime $Time12HrAgo -EndTime $EndTime -AggregationType Average -WarningAction SilentlyContinue
+    $Metrics12HrMax = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 12:00:00 -MetricName $MetricList -StartTime $Time12HrAgo -EndTime $EndTime -AggregationType Maximum -WarningAction SilentlyContinue
+    $Metrics12HrTot = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 12:00:00 -MetricName $MetricList -StartTime $Time12HrAgo -EndTime $EndTime -AggregationType Total -WarningAction SilentlyContinue
+    $Metrics1DayAvg = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 24:00:00 -MetricName $MetricList -StartTime $Time1DayAgo -EndTime $EndTime -AggregationType Average -WarningAction SilentlyContinue
+    $Metrics1DayMax = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 24:00:00 -MetricName $MetricList -StartTime $Time1DayAgo -EndTime $EndTime -AggregationType Maximum -WarningAction SilentlyContinue
+    $Metrics1DayTot = Get-AzureRMMetric -ResourceID $SingleVM.ID -timegrain 24:00:00 -MetricName $MetricList -StartTime $Time1DayAgo -EndTime $EndTime -AggregationType Total -WarningAction SilentlyContinue
+#CPU values are from [0] as it is first element in $MetricList
+    $CPUEntry | Add-Member -MemberType NoteProperty -Name "VMName" -Value $SingleVM.VMName
+    $CPUEntry | Add-Member -MemberType NoteProperty -Name "CPU_5_Min / Max" -value ($($Metrics5MinAvg[0].data.average).ToString("0.00") + " / " + $($Metrics5MinMax[0].data.maximum).ToString("0.00"))     
+    $CPUEntry | Add-Member -MemberType NoteProperty -Name "CPU_1_Hr / Max" -value ($($Metrics1HrAvg[0].data.average).ToString("0.00") + " / " + $($Metrics1HrMax[0].data.maximum).ToString("0.00"))
+    $CPUEntry | Add-Member -MemberType NoteProperty -Name "CPU_6_Hr / Max" -value ($($Metrics6HrAvg[0].data.average).ToString("0.00") + " / " + $($Metrics6HrMax[0].data.maximum).ToString("0.00"))
+    $CPUEntry | Add-Member -MemberType NoteProperty -Name "CPU_12_Hr / Max" -value ($($Metrics12HrAvg[0].data.average).ToString("0.00") + " / " + $($Metrics12HrMax[0].data.maximum).ToString("0.00"))
+    $CPUEntry | Add-Member -MemberType NoteProperty -Name "CPU_1_Day / Max" -value ($($Metrics1DayAvg[0].data.average).ToString("0.00") + " / " + $($Metrics1DayMax[0].data.maximum).ToString("0.00"))
+    $CPUEntry | Add-Member -MemberType NoteProperty -Name "CPU_30d Baseline" -value $($Baselines | where-object{$_.VM -eq $SingleVM.VMName -and $_.Metric -eq "CPU30d"} | select -expand "Value")
+    $My_CPU_OutputTable.Add($CPUEntry) | out-null
+#Net entries are from [1] and [2]
+    $NetInEntry | Add-Member -MemberType NoteProperty -Name "VMName" -Value $SingleVM.VMName
+    $NetInEntry | Add-Member -MemberType NoteProperty -Name "Net_5_Min In/BPS" -value ((ConvertBytes $Metrics5MinTot[1].data.total) + " / " + (ConvertBytes ($Metrics5MinTot[1].data.total / 300)))     
+    $NetInEntry | Add-Member -MemberType NoteProperty -Name "Net_1_Hr In/BPS" -value ((ConvertBytes $Metrics1HrTot[1].data.total) + " / " + (ConvertBytes ($Metrics1HrTot[1].data.total / 3600)))
+    $NetInEntry | Add-Member -MemberType NoteProperty -Name "Net_6_Hr In/BPS" -value ((ConvertBytes $Metrics6HrTot[1].data.total) + " / " + (ConvertBytes ($Metrics6HrTot[1].data.total / 21600)))
+    $NetInEntry | Add-Member -MemberType NoteProperty -Name "Net_12_Hr In/BPS" -value ((ConvertBytes $Metrics12HrTot[1].data.total) + " / " + (ConvertBytes ($Metrics12HrTot[1].data.total / 43200)))
+    $NetInEntry | Add-Member -MemberType NoteProperty -Name "Net_1_Day In/BPS" -value ((ConvertBytes $Metrics1DayTot[1].data.total) + " / " + (ConvertBytes ($Metrics1DayTot[1].data.total / 86400)))
+    $NetInEntry | Add-Member -MemberType NoteProperty -Name "Net_1d Baseline" -value (ConvertBytes ($Baselines | where-object{$_.VM -eq $SingleVM.VMName -and $_.Metric -eq "NetIn_1Day"} | select -expand "Value"))
+    $My_NetIn_OutputTable.Add($NetInEntry) | out-null
+    $NetOutEntry | Add-Member -MemberType NoteProperty -Name "VMName" -Value $SingleVM.VMName
+    $NetOutEntry | Add-Member -MemberType NoteProperty -Name "Net_5_Min Out/BPS" -value ((ConvertBytes $Metrics5MinTot[2].data.total) + " / " + (ConvertBytes ($Metrics5MinTot[2].data.total / 300)))     
+    $NetOutEntry | Add-Member -MemberType NoteProperty -Name "Net_1_Hr Out/BPS" -value ((ConvertBytes $Metrics1HrTot[2].data.total) + " / " + (ConvertBytes ($Metrics1HrTot[2].data.total / 3600)))
+    $NetOutEntry | Add-Member -MemberType NoteProperty -Name "Net_6_Hr Out/BPS" -value ((ConvertBytes $Metrics6HrTot[2].data.total) + " / " + (ConvertBytes ($Metrics6HrTot[2].data.total / 21600)))
+    $NetOutEntry | Add-Member -MemberType NoteProperty -Name "Net_12_Hr Out/BPS" -value ((ConvertBytes $Metrics12HrTot[2].data.total) + " / " + (ConvertBytes ($Metrics12HrTot[2].data.total / 43200)))
+    $NetOutEntry | Add-Member -MemberType NoteProperty -Name "Net_1_Day Out/BPS" -value ((ConvertBytes $Metrics1DayTot[2].data.total) + " / " + (ConvertBytes ($Metrics1DayTot[2].data.total / 86400)))
+    $NetOutEntry | Add-Member -MemberType NoteProperty -Name "Net_1d Baseline" -value (ConvertBytes ($Baselines | where-object{$_.VM -eq $SingleVM.VMName -and $_.Metric -eq "NetOut_1Day"} | select -expand "Value"))
+    $My_NetOut_OutputTable.Add($NetOutEntry) | out-null
 }
 Write-Host "`n"
 Write "`n" | Out-File C:\Temp\$($FileDate)_Azure_Status.txt -append
-$OutputTable | Format-Table -autosize | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+$My_CPU_OutputTable | Format-Table -autosize | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
 $NoteB | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
-
+$My_NetIn_OutputTable | Format-Table -autosize | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+$My_NetOut_OutputTable | Format-Table -autosize | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+$NoteC| Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
