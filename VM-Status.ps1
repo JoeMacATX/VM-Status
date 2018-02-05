e4811d693dfce979cb27ccbf8e7f8d76885ab478
JoeMacATX <36079229+JoeMacATX@users.noreply.github.com>
2018-02-05 11:14:18 -0600

    Added output functionality
    Added output functionality

--- VM-Status.ps1
+++ VM-Status.ps1
@@ -1,11 +1,13 @@
 ﻿az login                         
 az account set --subscription "Visual Studio Enterprise: BizSpark"
 
+Clear
 # This section contains baseline information relevant only to my subscription.  Can be modified/deleted as needed
 $NoteA = "Need to look at commonizing to minimal OS versions, i.e. migrate all Windows to DataCenter 2016"
 $NoteB = "30d Baseline values as of Jan 28, 2018"
 
-
+$FileDate = (Get-Date).ToString("yyyy-MM-dd_HHmm")
+Write $FileDate | Out-File C:\Temp\$($FileDate)_Azure_Status.txt -append
 
 Function AddToBaseline($name, $metric, $value)
 {
@@ -24,21 +26,23 @@ $Baselines += AddToBaseline 'WMReporting' 'CPU30d' 12.1
 
 # End notes section
 
-Clear
-Write-Host "VM-List"
-az vm list --query "[].{VMName:name, ResGroup:resourceGroup, OSType:storageProfile.imageReference.sku, Size:hardwareProfile.vmSize, Location:location, ID:id}"  --out table
+Write "VM-List" | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+$VMTableAzureOutput = az vm list --query "[].{VMName:name, ResGroup:resourceGroup, OSType:storageProfile.imageReference.sku, Size:hardwareProfile.vmSize, Location:location, ID:id}"  --out table
+$VMTableAzureOutput | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
 
-$VMListAzureOutput = az vm list --query "[].id"       #Use .id because pulling metrics requires ID value
 $MyVMList = @()
-for ($i = 1; $i -lt $VMListAzureOutput.Length - 1; $i++)     # Skips VMListAzureOutput[0] which is a left square bracket, and VMListAzureOutput[last] is a right square bracket
+for ($i = 2; $i -lt $VMTableAzureOutput.Length; $i++)         # First row of table is column headers [0], second row is "-"s [1]
 {
-   $MyVMList += $VMListAzureOutput[$i] -replace '[", ]',''   #$VMList contains the .ID values in one column
+   $SplitVals = $VMTableAzureOutput[$i] -split "\s+"
+   $MyVMList += $SplitVals[5]                                 #$VMList contains the .ID values in one column
 }
-Write-Host $NoteA
+Write "`n" | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+$NoteA | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
 
 
 $OutputTable = New-Object System.Collections.ArrayList
-Write-Host -NoNewline "`nCalculating CPU Usage."
+Write-Host -NoNewline "`nCalculating CPU Usage."              #Does not output to Text File
+Write "`n`nCalculating CPU Usage..." | Out-File C:\Temp\$($FileDate)_Azure_Status.txt -append
 foreach ($SingleVM in $MyVMList)                     #SingleVM is the .id value
 {
     $Entry = New-Object System.Object
@@ -93,17 +97,18 @@ foreach ($SingleVM in $MyVMList)                     #SingleVM is the .id value
     $DataMax = $DataMax -replace "[^0-9.]",''
     $OneDayCPU = [math]::round($DataAverage,2)
     $OneDayCPUMax = [math]::round($DataMax,2)
-    $Entry | Add-Member -MemberType NoteProperty -Name "VMName" -Value $VMName
-    $Entry | Add-Member -MemberType NoteProperty -Name "5MinCPU / Max" -value ("$($FiveMinCPU)".padleft(5) + " / " + "$($FiveMinCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "OneHrCPU / Max" -value ("$($OneHrCPU)".padleft(5) + " / " + "$($OneHrCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "SixHrCPU / Max" -value ("$($SixHrCPU)".padleft(5) + " / " + "$($SixHrCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "TwelveHrCPU / Max" -value ("$($TwelveHrCPU)".padleft(5) + " / " + "$($TwelveHrCPUMax)".padleft(5))
-    $Entry | Add-Member -MemberType NoteProperty -Name "OneDayCPU / Max" -value ("$($OneDayCPU)".padleft(5) + " / " + "$($OneDayCPUMax)".padleft(5))
+    $Entry | Add-Member -MemberType NoteProperty -Name "   VMName   " -Value $VMName
+    $Entry | Add-Member -MemberType NoteProperty -Name "5MinCPU / Max" -value ("$($FiveMinCPU)".padleft(8) + " / " + "$($FiveMinCPUMax)".padleft(5))
+    $Entry | Add-Member -MemberType NoteProperty -Name "OneHrCPU / Max" -value ("$($OneHrCPU)".padleft(8) + " / " + "$($OneHrCPUMax)".padleft(5))
+    $Entry | Add-Member -MemberType NoteProperty -Name "SixHrCPU / Max" -value ("$($SixHrCPU)".padleft(8) + " / " + "$($SixHrCPUMax)".padleft(5))
+    $Entry | Add-Member -MemberType NoteProperty -Name "TwelveHrCPU / Max" -value ("$($TwelveHrCPU)".padleft(8) + " / " + "$($TwelveHrCPUMax)".padleft(5))
+    $Entry | Add-Member -MemberType NoteProperty -Name "OneDayCPU / Max" -value ("$($OneDayCPU)".padleft(8) + " / " + "$($OneDayCPUMax)".padleft(5))
     $Entry | Add-Member -MemberType NoteProperty -Name "30d Baseline" -value $($Baselines | where-object{$_.VM -eq $VMName -and $_.Metric -eq "CPU30d"} | select -expand "Value")
     $OutPutTable.Add($Entry) | out-null
     Write-Host -NoNewline "."
 }
 Write-Host "`n"
-$OutputTable | Format-Table
-Write-Host $NoteB
+Write "`n" | Out-File C:\Temp\$($FileDate)_Azure_Status.txt -append
+$OutputTable | Format-Table -autosize | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
+$NoteB | Tee-Object -file C:\Temp\$($FileDate)_Azure_Status.txt -append
 
